cefed77258b7819d57ea873edee7f8ee27690453
Ibrahim Barry <ibarry7@terpmail.umd.edu>
2025-05-18 18:41:56 -0400

    Reverted it back to normal since modified version was not working

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const path = require('path');
 const supabaseClient = require('@supabase/supabase-js');
 const express = require('express');
 const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-
+const PORT = 3000;
 
 //Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'templates'));
@@ -21,51 +22,49 @@ const supabase = supabaseClient.createClient(supabaseURL, supabaseKey);
 
 //Routes to different pages
 app.get('/', (req, res) => {
-  res.render('steamapp');
+    res.render('steamapp');
 });
 app.get('/about', (req, res) => {
-  res.render('about');
+    res.render('about');
 });
 app.get('/games', (req, res) => {
-  res.render('games');
+    res.render('games');
 });
 
 //API - Attempts to get profiles from Supabase
-app.get('/api/profile', async (req, res) => {
- 
-  const { data, error } = await supabase.from('profile').select('*');
+app.get('/profile', async (req, res) => {
+    console.log('Attempting to GET all profile usernames')
 
-  if (error) {
-    console.error(`Error: ${error}`); 
-    return res.status(500).send(error); 
-  }
+    const { data, error } = await supabase.from('profile').select('*');
 
-  res.status(200).json(data);
-});
+    if(error) {
+        console.log(`Error: ${error}`);
+        res.send(error);
+    }
+
+    res.send(data)
+})
 
 //API - Attempts to add new profile
-app.post('/api/profile', async (req, res) => {
- 
-  const profileUser = req.body.profileUser;
+app.post('/profile', async (req, res) => {
+    console.log('Adding profile...')
 
-  const { data, error } = await supabase
+    console.log(req.body);
+    const profileUser = req.body.profileUser;
+
+    const { data, error } = await supabase
     .from('profile')
     .insert({ profile: profileUser })
     .select();
 
-  if (error) {
-    console.error(`Error: ${error}`); 
-    return res.status(500).send(error); 
-  }
+    if(error) {
+        console.log(`Error: ${error}`);
+        res.send(error);
+    }
 
-  res.status(201).json(data); 
+    res.send(data);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-
-module.exports = async (req, res) => {
-  try {
-    app(req, res);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Internal Server Error");
-  }
-};
